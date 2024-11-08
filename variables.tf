cd7df6aab35ea7f2c9f3a2069369f36f051070e7
Bode F <bode.falade@outlook.com>
2024-11-07 19:42:36 -0600

    added comments.

--- variables.tf
+++ variables.tf
@@ -1,16 +1,22 @@
+# Define the AWS region where the resources will be deployed
 variable "region" {
   default = "us-east-1"
 }
 
+# Name of the EKS cluster to be created
 variable "cluster_name" {
   default = "my-eks-cluster"
 }
 
+# ID of the VPC where the EKS cluster and Fargate profile will be created
 variable "vpc_id" {}
+
+# List of subnet IDs that the EKS cluster and Fargate profile will use
 variable "subnet_ids" {
   type = list(string)
 }
 
+# Namespace for the Fargate profile; defaults to "default"
 variable "fargate_namespace" {
   default = "default"
 }
